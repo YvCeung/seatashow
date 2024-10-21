25a2f42cc07f82d9a6fbb5de06a679bfdd94e830
zhangyu <zytuatua@163.com>
2024-10-22 00:26:37 +0800

    feature:support jdk21 build

--- seata-storage/src/main/java/com/zy/storage/SeataStorageApplication.java
+++ seata-storage/src/main/java/com/zy/storage/SeataStorageApplication.java
@@ -4,7 +4,7 @@ import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.zy.storage")
+@SpringBootApplication(scanBasePackages = {"com.zy.storage"})
 @EnableDubbo
 public class SeataStorageApplication {
 
